49c71d2e83c838886bc56c64690e6c276d2b79a5
lyslk <leslye.lk@gmail.com>
2020-01-03 01:48:16 +0100

    fix: add menu navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container } from 'semantic-ui-react'
+import NavBar from './components/menu';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Container>
+        <NavBar></NavBar>
+      </Container>
     </div>
   );
 }
